97ad8cd8f95a57e50628c3f8ca9361cdeb15782d
Royston L <roydl.net@gmail.com>
2017-03-13 19:15:24 +0530

    Process Scan and hash

--- client/win/appmon.py
+++ client/win/appmon.py
@@ -1,13 +1,28 @@
 import subprocess
+import re
 
 class AppMon:
+
+    processList = []
+
     def __init__(self):
         print("hello")
 
     def list(self):
-
-        cmd = 'WMIC PROCESS get Caption,Processid'
+        cmd = 'WMIC PROCESS get Processid,ExecutablePath'
         proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         for line in proc.stdout:
-            print(line)
+            process = re.split("  +", line.decode("utf-8"))
+            self.processList.append(process)
 
+
+
+        for process in self.processList:
+
+            if len(process[0]) > 0:
+                print("Hashing " + process[0])
+                cmd = 'certUtil -hashfile "' + process[0] + '" md5 | find /v "hash of file" | find /v "Cert"'
+
+                proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+                for line in proc.stdout:
+                    print(line.decode("UTF-8").replace(" ", ""))
