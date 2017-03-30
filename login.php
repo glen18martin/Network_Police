88f47a9b674376612631dfbb0c20239d92223d9a
Glen Martin <glen18martin@gmail.com>
2017-03-31 00:21:24 +0530

    databse and link

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'config.php';
+
+session_start();
+	
+//if ($_POST['form']==0) 
+
+
+//else  {
+
+
+ $checkid = $_POST['u'];
+ $checkpass = $_POST['p'];
+
+$checkpass = md5($checkpass);
+
+$result = mysql_query("SELECT user_id FROM user WHERE username = '$checkid' AND password = '$checkpass'");
+
+if(mysql_num_rows($result) == 0) {
+     echo "Check username or password";
+
+//changes
+
+     header("Location:index.html");
+
+     //changesend
+
+} else {
+   // echo "Found user Successfully";
+  
+header("Location:main.html");
+
+
+
+
+$row = mysql_fetch_array($result);
+$id = $row['user_id'];
+$_SESSION["login"]="$id";
+$checklog = $id;
+
+}
+
+//}
+
+
+$_SESSION["checklog"]="$checklog";
+
+
+
+?>
