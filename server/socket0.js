bb39b77da9f7261bdbe05dfe60a24b5350b6bc1d
roydl7 <roydl.net@gmail.com>
2017-04-06 23:59:47 +0530

    Ethernet

--- server/socket0.js
+++ server/socket0.js
@@ -47,7 +47,7 @@ io.on('connection', function(socket) {
     setInterval(function() {
         socket.emit('get_disk_usage');
         socket.emit('get_memory_usage');
-        
+        socket.emit('get_enetwork_usage');
         socket.emit('get_network_usage');
         socket.emit('get_cpu_usage');
         //console.log(JSON.stringify(clientList));
@@ -76,7 +76,7 @@ io.on('connection', function(socket) {
     
     socket.on('ident', function(data) {
         console.log("Online " + data);
-        clientList[socket] = { id: data, du: 0, mu: 0, nu: 0, cu:0, pr:0};
+        clientList[socket] = { id: data, du: 0, mu: 0, nu: 0, cu:0, pr:0, enu: 0};
     });
 
     
@@ -90,6 +90,10 @@ io.on('connection', function(socket) {
     });
     socket.on('network_usage_response', function(data) {
         clientList[socket].nu = data;
+        
+    });
+    socket.on('enetwork_usage_response', function(data) {
+        clientList[socket].enu = data;
         
     });
     socket.on('cpu_usage_response', function(data) {
