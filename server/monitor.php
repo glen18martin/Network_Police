b81c25615563845f3694b5c5299dcb8321811de6
roydl7 <roydl.net@gmail.com>
2017-04-07 10:19:45 +0530

    block table matching

--- server/monitor.php
+++ server/monitor.php
@@ -1,7 +1,18 @@
 <?php
-
+require '../config.php';
 $dump = file_get_contents("client.dump");
 $obj = json_decode($dump, true);
+
+
+$hashes = array();
+$result = mysql_query("SELECT * FROM app_hash");
+while($row = mysql_fetch_array($result)) {
+    $hashes[] = $row['hash']; 
+}
+
+
+
+
 
 $offenders = [];
 
@@ -13,9 +24,17 @@ for($id = 0; $id < count($obj); $id++) {
         $path =  array_values($obj)[$id]['pr'][$i]['path'];
         $hash = array_values($obj)[$id]['pr'][$i]['hash'];
         $idx =  array_values($obj)[$id]["id"];
+
+        foreach($hashes as $chash) {
+            if($hash == $chash) {
+                $offenders[] = array("id" => $idx, "exe" => $path, "pid" => $pid); 
+            }
+        }
+
+        /*
         if(strpos($path, "vlc.exe") > 0) {  //if($hash == '70b2f403924958b8bd9bcace8e097a59') {
             $offenders[] = array("id" => $idx, "exe" => $path, "pid" => $pid); 
-        }
+        }*/
     }
 }
 
